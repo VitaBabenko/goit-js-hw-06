53815dfa9cd7620476b3241466fc248771709d2e
VitaBabenko <babenkovita1988@gmail.com>
2022-12-08 22:29:12 +0200

    changed files

--- task-07.js
+++
@@ -1,9 +0,0 @@
-const input = document.querySelector('#font-size-control');
-const span = document.querySelector('#text');
-
-input.addEventListener('input', onInputChange);
-
-function onInputChange(event) {
-    const { value } = event.currentTarget;
-    span.style.fontSize = `${value}px`;
-}
